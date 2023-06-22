9b8daf9b818a06cc36cae826d84365f4eba643b8
edouard <m@edouard.paris>
2023-06-22 09:30:53 +0200

    Add the basics from git.peppe.rs

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Neovim powered Journaling tool";
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs";
+    };
+    neovim = {
+      url = "github:neovim/neovim/stable?dir=contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    neovim,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    f = "%Y/%m";
+  in {
+    packages.x86_64-linux.default =
+      # starts nvim with 2 months of journal entries ahead and behind
+      # nvim --cmd 'source .nvimrc' -O 2023/10 2023/11 2023/12 2024/01 2024/02
+      pkgs.writeScriptBin "jrnl" ''
+        nvim --cmd 'source .nvimrc' -O $(
+          ${pkgs.dateutils}/bin/dateseq \
+              "$(date --date "2 months ago" +${f})" \
+              "$(date --date "2 months" +${f})" \
+              -i ${f} \
+              -f ${f}
+        )
+      '';
+
+    devShell.x86_64-linux =
+      pkgs.mkShell
+      {
+        nativeBuildInputs = [
+          self.packages.x86_64-linux.default
+        ];
+      };
+  };
+}
