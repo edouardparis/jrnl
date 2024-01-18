d23164c1fb16caf3e1affbc96b9266e6bd44efb7
edouardparis <m@edouard.paris>
2024-01-18 10:37:24 +0100

    Add proper installPhase

--- flake.nix
+++ flake.nix
@@ -1,36 +1,44 @@
 {
   description = "Neovim powered Journaling tool";
+
   inputs = {
     nixpkgs = {
       url = "github:NixOS/nixpkgs";
     };
   };
+
   outputs = {
     self,
     nixpkgs,
+    ...
   }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
     f = "%Y/%m";
   in {
-    packages.x86_64-linux.default =
-      # starts nvim with 2 months of journal entries ahead and behind
-      # nvim --cmd 'source .nvimrc' -O 2023/10 2023/11 2023/12 2024/01 2024/02
-      pkgs.writeScriptBin "journal" ''
-        ${pkgs.neovim}/bin/nvim --cmd 'source ~/flakes/jrnl/.nvimrc' -O $(
+    packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+      name = "journal";
+      src = self;
+      buildInputs = [pkgs.neovim pkgs.dateutils];
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp ${./.nvimrc} $out/.nvimrc
+        cat > $out/bin/journal <<EOF
+        #!/usr/bin/env bash
+        ${pkgs.neovim}/bin/nvim --cmd 'source $out/.nvimrc' -O \$(
           ${pkgs.dateutils}/bin/dateseq \
-              "$(date --date "2 months ago" +${f})" \
-              "$(date --date "2 months" +${f})" \
+              "\$(date --date "2 months ago" +${f})" \
+              "\$(date --date "2 months" +${f})" \
               -i ${f} \
               -f ${f}
         )
+        EOF
+        chmod +x $out/bin/journal
       '';
+    };
 
-    devShell.x86_64-linux =
-      pkgs.mkShell
-      {
-        nativeBuildInputs = [
-          self.packages.x86_64-linux.default
-        ];
-      };
+    devShell.x86_64-linux = pkgs.mkShell {
+      nativeBuildInputs = [self.packages.x86_64-linux.default];
+    };
   };
 }
